50380479f05c19cc14f1c0548a17714c13cfdb11
Nick Wey <nicholas.ph.wey@gmail.com>
2021-12-10 16:34:33 -0500

    Delete client directory

--- client/parser/ParseException.scala
+++
@@ -1,8 +0,0 @@
-package guizilla.sol.client.parser
-import sparkzilla.src.Token
-
-/**
- * An exception thrown when there is an unexpected error in the tokenizer
- */
-class ParseException(expected: String)
-  extends Exception("Error in parsing, expected: " + expected + ".")
