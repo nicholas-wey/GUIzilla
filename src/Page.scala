f72e53846d5bb8b953040a3d034dc16420673ec7
Nick Wey <nicholas.ph.wey@gmail.com>
2021-12-10 16:30:22 -0500

    Add files via upload

---
+++ src/Page.scala
@@ -0,0 +1,9 @@
+package guizilla.src
+
+abstract class Page extends Cloneable {
+
+  override def clone = super.clone.asInstanceOf[Page]
+
+  def defaultHandler(inputs: Map[String, String], sessionId: String): String
+
+}
